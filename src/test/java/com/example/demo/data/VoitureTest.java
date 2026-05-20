7ce4adfa062d3f3bccc8e5a8155b8d4e43be3e8d
sarah <sarahhellal05@gmail.com>
2026-05-20 11:55:34 +0200

    ajout tests voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -10,8 +10,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void creerVoitureAvecConstructeurVide() {
+        Voiture voiture = new Voiture();
+
+        voiture.setMarque("Renault");
+        voiture.setPrix(15000);
+        voiture.setId(1);
+
+        assertEquals("Renault", voiture.getMarque());
+        assertEquals(15000, voiture.getPrix());
+        assertEquals(1, voiture.getId());
     }
 
+    @Test
+    void creerVoitureAvecConstructeurMarquePrix() {
+        Voiture voiture = new Voiture("Peugeot", 20000);
+
+        assertEquals("Peugeot", voiture.getMarque());
+        assertEquals(20000, voiture.getPrix());
+    }
+
+    @Test
+    void modifierVoitureAvecSetters() {
+        Voiture voiture = new Voiture();
+
+        voiture.setMarque("Citroen");
+        voiture.setPrix(12000);
+        voiture.setId(5);
+
+        assertEquals("Citroen", voiture.getMarque());
+        assertEquals(12000, voiture.getPrix());
+        assertEquals(5, voiture.getId());
+    }
+
+    @Test
+    void testerToString() {
+        Voiture voiture = new Voiture("Fiat", 10000);
+        voiture.setId(2);
+
+        assertEquals("Car{marque='Fiat', prix=10000, id=2}", voiture.toString());
+    }
 }
